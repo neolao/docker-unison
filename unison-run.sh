2928de5a948348eccf58bf240cb232c68494494a
neolao <contact@neolao.com>
2016-01-12 17:19:03 +0100

    Update unison-run.sh

--- unison-run.sh
+++ unison-run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 export HOME=/root
-cd /unison
+
+if [ ! -e "$UNISON_VOLUME" ]
+then
+    mkdir $UNISON_VOLUME
+fi
+
+cd $UNISON_VOLUME || exit
 exec unison-$UNISON_VERSION -socket 5000 >>/var/log/unison.log 2>&1
